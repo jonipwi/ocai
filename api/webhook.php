8111e66c25b269a1815d4d997dbfffe9c87e1c14
jonipwi <joni.pwi@gmail.com>
2023-05-29 17:28:36 +0700

    joni:latest

--- api/webhook.php
+++ api/webhook.php
@@ -10,6 +10,10 @@ $fp = file_put_contents('./hook.log', $req_dump );
 if($json = json_decode(file_get_contents("php://input"), true)){
    $data = $json;
 }
-print_r($data);
+//print_r($data);
 
+$encoded = $data;
+$myJSON = json_encode($encoded);
+header('Content-type: application/json; charset=utf-8');
+exit($myJSON);
 ?>
