c3bd5f105017e67889ab8375af6b19e938aa6102
jonipwi <joni.pwi@gmail.com>
2023-05-29 17:06:36 +0700

    joni:latest

--- api/webhook.php
+++ api/webhook.php
@@ -1,3 +1,15 @@
 <?php 
 
+// Original Answer
+header('Content-Type: application/json');
+$request = file_get_contents('php://input');
+$req_dump = print_r( $request, true );
+$fp = file_put_contents('./hook.log', $req_dump );
+
+// Updated Answer
+if($json = json_decode(file_get_contents("php://input"), true)){
+   $data = $json;
+}
+print_r($data);
+
 ?>
