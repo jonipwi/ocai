5296e4d8b26bf7d79a95277c62f6933a14e94166
jonipwi <joni.pwi@gmail.com>
2023-05-29 18:57:56 +0700

    joni:latest

--- flutter/lib/globals.dart
+++ flutter/lib/globals.dart
@@ -4,3 +4,4 @@ String? Result = '';
 
 String? titlePage = 'Project 68AP';
 int goToPage = 0;
+String? versi = '1.0.0+1';
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -70,70 +70,100 @@ class _MyHomePageState extends State<MyHomePage> {
           callPage(0, 'Ocai');
         },
         child: Stack(
-          children: [
-            Positioned(
-              top: outrange,
-              left: 0,
-              child: Container(
-                color: Colors.white,
-                width: width,
-                height: height - outrange,
-                child: SingleChildScrollView(
-                  physics: ScrollPhysics(),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Welcome Ocai',
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                        Text(
-                          'Project 68AP',
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                        SizedBox(height: 10),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                          //child: Flexible(
+            children: [
+              Positioned(
+                top: outrange,
+                left: 0,
+                child: Container(
+                  color: Colors.white,
+                  width: width,
+                  height: height - outrange,
+                  child: SingleChildScrollView(
+                    physics: ScrollPhysics(),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Welcome Ocai',
+                            style: Theme.of(context).textTheme.headlineMedium,
+                          ),
+                          Text(
+                            'Project 68AP',
+                            style: Theme.of(context).textTheme.headlineMedium,
+                          ),
+                          SizedBox(height: 10),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                            //child: Flexible(
                             child: Text('Introducing "Project 68AP" - Unleashing the Power of Self-Evaluation for Church and Personal Growth!'),
-                          //),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                          //child: Flexible(
+                            //),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                            //child: Flexible(
                             child: Text('Step into a groundbreaking project that will revolutionize the way churches assess their spiritual progress, bridging the gap between the physical and the divine. Prepare to embark on an exhilarating journey of self-discovery and transformation as you evaluate your position on the path to enlightenment using our cutting-edge indicator.'),
-                          //),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                          //child: Flexible(
+                            //),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                            //child: Flexible(
                             child: Text('Gone are the days of uncertainty and guesswork; our meticulously crafted mathematic formula, based on the standard deviation, will serve as your compass towards a better life. We are proud to present a new and attainable approach that empowers both churches and individuals to unlock their full potential.'),
-                          //),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                          //child: Flexible(
+                            //),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                            //child: Flexible(
                             child: Text('Dare to dream bigger, for we firmly believe that with our revolutionary method, each and every one of you has the potential to achieve a remarkable 68% or more from the coveted target point. Allow us to unveil the extraordinary "Project 68AP" - a catalyst for personal growth and a testament to the incredible heights you can reach.'),
-                          //),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
-                          //child: Flexible(
+                            //),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                            //child: Flexible(
                             child: Text('For in this endeavor, we honor the Kingdom, embrace the Power, and celebrate the everlasting Glory. Join us on this awe-inspiring expedition, where miracles become reality. Forever Amen!'),
-                          //),
-                        ),
+                            //),
+                          ),
 
-                        Divider(),
-                        Center( child: Text('(C)opyright 2023. Ocai Team & Community')),
-                        Center( child: Text('https://github.com/jonipwi/ocai.git')),
+                          Divider(),
+                          Center( child: Text('(C)opyright 2023. Ocai Team & Community')),
+                          Center( child: Text('https://github.com/jonipwi/ocai.git')),
 
-                        SizedBox(height: 100),
-                      ]),
+                          SizedBox(height: 100),
+                        ]),
                   ),
                 ),
               ),
 
-            (goToPage == 66) ? Positioned(
+              (goToPage == 66) ? Positioned(
+                bottom: 15,
+                left: 20,
+                child: Container(
+                    width: width - 40 - 66,
+                    height: 55,
+                    //color: Color.fromRGBO(250, 250, 250, 0.9),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(240, 250, 240, 0.9),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Container(
+
+                    )
+                ),
+              ) : Container(),
+
+              (goToPage == 66) ? Positioned(
                 top: outrange + 20,
                 left: 20,
                 child: Container(
@@ -160,31 +190,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: SingleChildScrollView(
                       physics: ScrollPhysics(),
                       child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
                               width: width - 40,
                               height: 36,
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(50, 100, 50, 0.8),
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(10),
-                                      topRight: Radius.circular(10),
-                                      //bottomLeft: Radius.circular(10),
-                                      //bottomRight: Radius.circular(10)
-                                  ),
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(50, 100, 50, 0.8),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                  //bottomLeft: Radius.circular(10),
+                                  //bottomRight: Radius.circular(10)
                                 ),
-                                child: Padding(
+                              ),
+                              child: Padding(
                                 padding: EdgeInsets.only(top: 5, bottom: 5),
                                 child: Center(
                                   child: Text('${titlePage}', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
                                 ),
                               )
-                            ),
+                          ),
 
-                            Padding(
-                                padding: EdgeInsets.only(top: 5, bottom: 0, left: 5, right: 5),
-                                child: Column(
+                          Padding(
+                              padding: EdgeInsets.only(top: 5, bottom: 0, left: 5, right: 5),
+                              child: Column(
                                 children: [
                                   Row(
                                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -199,52 +229,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
                                 ],
                               )
-                            ),
+                          ),
 
-                            (Question!.length > 0) ? Padding(
-                                padding: EdgeInsets.only(top: 5, bottom: 0, left: 5, right: 5),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Container(
-                                          child: Text('${Question}'),
-                                        ),
-                                        Icon(Icons.account_circle_rounded, color: Colors.blueGrey),
-                                      ],
-                                    ),
+                          (Question!.length > 0) ? Padding(
+                              padding: EdgeInsets.only(top: 5, bottom: 0, left: 5, right: 5),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Container(
+                                        child: Text('${Question}'),
+                                      ),
+                                      Icon(Icons.account_circle_rounded, color: Colors.blueGrey),
+                                    ],
+                                  ),
 
-                                  ],
-                                )
-                            ) : Container(),
+                                ],
+                              )
+                          ) : Container(),
 
-                            (Answer!.length > 0) ? Padding(
-                                padding: EdgeInsets.only(top: 5, bottom: 5, left: 5, right: 5),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Icon(Icons.circle_notifications, color: Colors.blueGrey),
-                                        Container(
-                                          child: Text('${Answer}'),
-                                        ),
-                                      ],
-                                    ),
+                          (Answer!.length > 0) ? Padding(
+                              padding: EdgeInsets.only(top: 5, bottom: 5, left: 5, right: 5),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Icon(Icons.circle_notifications, color: Colors.blueGrey),
+                                      Container(
+                                        child: Text('${Answer}'),
+                                      ),
+                                    ],
+                                  ),
 
-                                  ],
-                                )
-                            ) : Container(),
+                                ],
+                              )
+                          ) : Container(),
 
-                          ],
+                        ],
                       )
                   ),
                 ),
-            ): Container(),
-          ]),
+              ): Container(),
+            ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: GPTDialogue,
